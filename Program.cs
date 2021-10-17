37d229bac65163988ee3ba60c7ea76ab3a3343ea
William <capuletos@live.com>
2021-10-17 17:06:28 -0300

    ReadLaboratories Created

--- Program.cs
+++ Program.cs
@@ -1,4 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Data.SqlClient;
+using Dapper;
+using Dapper.Contrib.Extensions;
+using Pharmaceuticals.Models;
+
 Console.WriteLine("///");
 
+const string CONNECTION_STRING = @"Data Source=(localdb)\\MSSQLLocalDB;Database=Laboratory;Trusted_Connection=True;MultipleActiveResultSets=True;";
+
+static void ReadLaboratories()
+{
+    using (var connection = new SqlConnection(CONNECTION_STRING))
+    {
+        var laboratories = connection.GetAll<Laboratory>();
+
+        foreach (var laboratory in laboratories)
+        {
+            Console.WriteLine(laboratory.Name);
+        }
+    }
+
+}
 
